06f860771f85444e7968b7003c799cc08b7212ee
kcruz95 <kyle.cruz@hotmail.ca>
2021-07-22 17:11:48 -0700

    parameterised

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString = `
+SELECT DISTINCT (teachers.name) AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+  `;
 
-console.log(res.rows);
+const cohortName = process.argv[2] || 'JUL02';
+// const limit = process.argv[3] || 5;
+// // Store all potentially malicious values in an array.
+// const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, cohortName)
+.then(res => {
+  res.rows.forEach(teacher => {
+    console.log(`${teacher.cohort}: ${teacher.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
